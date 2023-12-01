1fdf4ec269595dffe9b1f3dd6eb6c0315c140016
bizhen <35422945+bizhen46766@users.noreply.github.com>
2023-12-01 22:19:08 +0800

    Update pretrain_umls.sh

--- pretrain/scripts/pretrain_umls.sh
+++ pretrain/scripts/pretrain_umls.sh
@@ -1,15 +1,16 @@
-DATA_DIR="/home/bz/Relphormer-github/dataset/umls"
-nohup python -u main.py --gpus "1" --max_epochs=30  --num_workers=32 \
-   --model_name_or_path  bert-base-uncased \
+DATA_DIR="../dataset/umls"
+PRETRAINED_MODEL_PATH="../Pre-trained_models/bert-base-uncased"
+CUDA_VISIBLE_DEVICES=0 nohup python -u main.py --gpus "1" --max_epochs=30  --num_workers=32 \
+   --model_name_or_path  ${PRETRAINED_MODEL_PATH} \
    --accumulate_grad_batches 1 \
+   --bce 0 \
    --model_class BertKGC \
    --batch_size 256 \
    --pretrain 1 \
-   --bce 0 \
    --check_val_every_n_epoch 1 \
+   --data_dir ${DATA_DIR} \
    --overwrite_cache \
-   --data_dir ${DATA_DIR}\
    --eval_batch_size 256 \
-   --max_seq_length 64 \
+   --max_seq_length 32 \
    --lr 1e-4 \
    >logs/pretrain_umls.log 2>&1 &
