dd641d32d92abff7e4111501b94cde5afb07fe41
bizhen <35422945+bizhen46766@users.noreply.github.com>
2024-01-22 00:36:15 +0800

    Update fb15k.sh

--- scripts/fb15k-237/fb15k.sh
+++ scripts/fb15k-237/fb15k.sh
@@ -3,12 +3,12 @@ python main.py --gpus "2," --max_epochs=25  --num_workers=32 \
    --accumulate_grad_batches 1 \
    --model_class Relphormer \
    --batch_size 128 \
-   --checkpoint output_pretrained/FB15k-237/epoch=15-step=19299-Eval/hits10=0.93.ckpt \
+   --checkpoint output_pretrained/fb15k-237/epoch=15-step=19299-Eval/hits10=0.93.ckpt \
    --pretrain 0 \
    --bce 0 \
    --check_val_every_n_epoch 1 \
    --overwrite_cache \
-   --data_dir dataset/FB15k-237 \
+   --data_dir dataset/fb15k-237 \
    --eval_batch_size 256 \
    --max_seq_length 64 \
    --max_triplet 5 \
