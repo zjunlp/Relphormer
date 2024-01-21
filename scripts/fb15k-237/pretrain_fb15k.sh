383a02d862652914f86ff19feec133f79544173b
bizhen <35422945+bizhen46766@users.noreply.github.com>
2024-01-22 00:35:59 +0800

    Update pretrain_fb15k.sh

--- scripts/fb15k-237/pretrain_fb15k.sh
+++ scripts/fb15k-237/pretrain_fb15k.sh
@@ -7,7 +7,7 @@ python main.py --gpus "0," --max_epochs=16  --num_workers=32 \
    --bce 0 \
    --check_val_every_n_epoch 1 \
    --overwrite_cache \
-   --data_dir dataset/FB15k-237 \
+   --data_dir dataset/fb15k-237 \
    --eval_batch_size 256 \
    --max_seq_length 64 \
    --lr 3e-5
