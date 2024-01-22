3bb6ade2ac233dbc7b83ec356dd665fd40548963
bizhen <35422945+bizhen46766@users.noreply.github.com>
2024-01-22 11:56:51 +0800

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,6 +1,15 @@
-# from transformers.models.bert.modeling_bert import BertForMaskedLM
-from models.huggingface_relformer import BertForMaskedLM
+from transformers.models.bert.modeling_bert import BertForMaskedLM
+
+
 class BertKGC(BertForMaskedLM):
+    @staticmethod
+    def add_to_argparse(parser):
+        parser.add_argument("--pretrain", type=int, default=0, help="")
+        return parser
+
+
+from models.huggingface_relformer import BertForMaskedLM as BertForMaskedLM_
+class Relphormer(BertForMaskedLM_):
     @staticmethod
     def add_to_argparse(parser):
         parser.add_argument("--pretrain", type=int, default=0, help="")
